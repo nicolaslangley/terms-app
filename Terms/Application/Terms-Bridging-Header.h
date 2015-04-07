f7ee7e45224775ef0eb2ecf628f599100284704a
Nicolas Langley <nicolasqlangley@gmail.com>
2015-04-06 23:10:42 -0700

    Removed old Moxtra dependencies and simplified bridging header

--- Terms/Application/Terms-Bridging-Header.h
+++ Terms/Application/Terms-Bridging-Header.h
@@ -9,35 +9,7 @@
 #ifndef ProjectAlpha_Terms_Bridging_Header_h
 #define ProjectAlpha_Terms_Bridging_Header_h
 
-@import Accelerate;
-@import AddressBook;
-@import AddressBookUI;
-@import AssetsLibrary;
-@import AudioToolbox;
-@import AVFoundation;
-@import CFNetwork;
-@import CoreBluetooth;
-@import CoreGraphics;
-@import CoreImage;
-@import CoreLocation;
-@import CoreMedia;
-@import CoreText;
-@import CoreVideo;
-@import EventKit;
-@import Foundation;
-@import ImageIO;
-@import MapKit;
-@import MediaPlayer;
-@import MessageUI;
-@import MobileCoreServices;
-@import OpenGLES;
-@import QuartzCore;
-@import Security;
-@import StoreKit;
-@import SystemConfiguration;
-@import UIKit;
-@import CoreTelephony;
 
-//#import "Moxtra.h"
+@import UIKit;
 
 #endif
